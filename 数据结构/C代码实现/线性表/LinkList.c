8908c86b963349e14842497f038c2821cb44011e
Zee-hash <62379579+Zee-hash@users.noreply.github.com>
2020-08-07 20:31:34 +0800

    20200807

--- 数据结构/C代码实现/线性表/LinkList.c
+++ 数据结构/C代码实现/线性表/LinkList.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 typedef int ElemType;
 
-// LinkList -> LNode -> LNode -> ··· -> LNode -> NULL
+// HeadLink(保存HeadNode的地址) -> HeadNode -> LNode -> LNode -> ··· -> LNode -> NULL
 
 typedef struct LNode{
     ElemType data;// 数据域
@@ -54,4 +55,125 @@ LinkList List_TailInsert(LinkList L)
     // 尾结点指针域置空
     r->next = NULL;
     return L;
+}
+
+// 按序号查找元素，查找成功则返回指向第i个结点的指针，否则返回NULL
+LNode *GetElem(LinkList L, int i)
+{
+    int j = 1;
+    LNode *p = L->next;// p指向头结点
+    if(i == 0)
+    {
+        return p;
+    }
+    if(i < 1)
+    {
+        return NULL;// 查找位置无效
+    }
+    while(p && j < i)
+    {
+        p = p->next;
+        j++;
+    }
+    return p;
+}
+
+// 按值查找元素，查找成功则返回该结点的指针，否则返回NULL
+LNode *LocateElem(LinkList L, ElemType e)
+{
+    LNode *p = L->next;// 头结点
+    while(p->next && p->data != e)
+    {
+        p = p->next;// 继续往后查找
+    }
+    return p;
+}
+
+// 返回链表长度，长度为0表示链表仅含头结点
+int ListLength(LinkList L)
+{
+    int length = 0;
+    LNode *p = L->next;
+    while(p)
+    {
+        p = p->next;
+        length++;
+    }
+    return length;
+}
+
+// 在链表的第i个位置插入一个新的结点
+LNode *ListInsert(LinkList L, int i, ElemType e)
+{
+    // 检查插入位置的合法性
+    if(i < 1 || i > ListLength(L) + 1)
+    {
+        printf("插入位置有误\n");
+        return L;// 插入位置错误，原样返回
+    }
+    LNode *s = (LinkList)malloc(sizeof(LNode));
+    s->data = e;
+    LNode *p;
+    if((p = GetElem(L,i-1)))
+    {
+        s->next = p->next;
+        p->next = s;
+    }
+    return L;
+}
+
+// 删除链表中第i个位置的结点，并通过e返回该结点的信息
+LNode *ListDelete(LinkList L, int i, ElemType *e)
+{
+    // 检查删除位置的合法性
+    if(i < 1 || i > ListLength(L))
+    {
+        printf("删除位置有误\n");
+        return L;// 删除位置错误，原样返回
+    }
+    // 保存结点信息
+    *e = GetElem(L, i)->data;
+    LNode *p = GetElem(L, i-1);
+    LNode *q = NULL;
+    if(p)
+    {
+        q = p->next;
+        p->next = q->next;
+        free(q);
+    }
+    return L;
+}
+
+void ShowLinkListInfo(LinkList L)
+{
+    int length = ListLength(L);
+    if(length == 0)
+    {
+        printf("L(&HeadNode)-->NULL\n");
+    }
+    if(length > 0)
+    {
+        LNode *p = L->next;
+        printf("L(&HeadNode)");
+        while(p->next)
+        {
+            printf("-->%d", p->data);
+            p = p->next;
+        }
+        printf("-->%d-->NULL", p->data);
+        printf("\nLength of LinkList : %d\n", length);
+    }
+}
+
+int main()
+{
+    LinkList L = NULL;
+    L = List_HeadInsert(L);
+    ShowLinkListInfo(L);
+    L = ListInsert(L, 2, 9);
+    ShowLinkListInfo(L);
+    ElemType e = 0;
+    L = ListDelete(L, 3, &e);
+    printf("element %d has been deleted.\n", e);
+    ShowLinkListInfo(L);
 }
